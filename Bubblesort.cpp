d55c7444daf5f50b2b6375d1bbd8151aadfa7383
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-08-01 18:28:20 +0300

    adding the swap function

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
+void swap(int &x, int &y){
+    int temporary = x;
+    x = y;
+    y = temporary;
+}
+
 void sort(int array[], int size){
     int temporary;
     for (int i = 0; i < size - 1; i++){
         for (int j = 0; j < size - i - 1; j++){
             if(array[j] > array[j+1]){
-                temporary = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temporary;
+                swap(array[j], array[j + 1]);
             }
         }
     }
